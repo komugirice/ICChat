c636617768b62f33e2be1f369ab596ab88a7cbe0
komugirice <iys60111118082@gmail.com>
2019-11-26 20:29:22 +0900

    ログイン機能

--- app/src/main/java/com/komugirice/icchat/MainActivity.kt
+++ app/src/main/java/com/komugirice/icchat/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.komugirice.icchat
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.komugirice.icchat.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    companion object {
+        fun start(activity: Activity) = activity.startActivity(Intent(activity, MainActivity::class.java))
     }
 }
