7a6295096195c0352522eff79962f68ddad8a4cb
komugirice <iys60111118082@gmail.com>
2019-11-28 17:55:38 +0900

    TabLayout作成

--- app/src/main/java/com/komugirice/icchat/MainActivity.kt
+++ app/src/main/java/com/komugirice/icchat/MainActivity.kt
@@ -1,17 +1,122 @@
 package com.komugirice.icchat
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.komugirice.icchat.ui.login.LoginActivity
+import androidx.annotation.ColorRes
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT
+import androidx.viewpager.widget.ViewPager
+import com.komugirice.icchat.fragment.DebugFragment
+import com.komugirice.icchat.fragment.FriendFragment
+import com.komugirice.icchat.fragment.InterestFragment
+import com.komugirice.icchat.fragment.RoomFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    // TabLayoutで使用
+    private val customAdapter by lazy { CustomAdapter(this, supportFragmentManager, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) }
+
+    /**
+     * onCreateメソッド
+     *
+     * @param savedInstanceState
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initialize()
     }
+
+    /**
+     * initializeメソッド
+     *
+     */
+    private fun initialize() {
+        initLayout()
+    }
+
+    /**
+     * initLayoutメソッド
+     *
+     */
+    private fun initLayout() {
+        initClick()
+        initViewPager()
+        initTabLayout()
+    }
+
+    /**
+     * initClickメソッド
+     *
+     */
+    private fun initClick() {
+
+    }
+
+    /**
+     * initViewPagerメソッド
+     *
+     */
+    private fun initViewPager() {
+        viewPager.apply {
+            adapter = customAdapter
+            offscreenPageLimit = customAdapter.count
+            addOnPageChangeListener(object: ViewPager.OnPageChangeListener {
+                override fun onPageScrollStateChanged(state: Int) {}
+                override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {}
+                override fun onPageSelected(position: Int) {
+                }
+            })
+        }
+    }
+
+    /**
+     * initTabLayoutメソッド
+     *
+     */
+    private fun initTabLayout() {
+        tabLayout.setupWithViewPager(viewPager)
+//        tabLayout.getTabAt(0)?.setIcon(R.drawable.ic_person_white_48dp)
+//        tabLayout.getTabAt(1)?.setIcon(R.drawable.ic_chat_white_48dp)
+//        tabLayout.getTabAt(2)?.setIcon(R.drawable.ic_important_devices_white_48dp)
+//        tabLayout.getTabAt(3)?.setText(R.string.tab_debug)
+        tabLayout.getTabAt(0)?.setCustomView(R.layout.design_fragment_icon_person)
+        tabLayout.getTabAt(1)?.setCustomView(R.layout.design_fragment_icon_chat)
+        tabLayout.getTabAt(2)?.setCustomView(R.layout.design_fragment_icon_interest)
+        tabLayout.getTabAt(3)?.setText(R.string.tab_debug)
+    }
+
+    /**
+     * CustomAdapterクラス
+     * TabLayout用
+     *
+     */
+    class CustomAdapter(private val context: Context, fragmentManager: FragmentManager, behavor: Int) :
+        FragmentPagerAdapter(fragmentManager, behavor) {
+
+        inner class Item(val fragment: Fragment, val title:Int)
+
+        val fragments = listOf(Item(FriendFragment(), R.string.tab_frient)
+            , Item(RoomFragment(), R.string.tab_room)
+            , Item(InterestFragment(), R.string.tab_interest)
+            , Item(DebugFragment(), R.string.tab_debug))
+
+        override fun getCount(): Int = fragments.size
+
+        override fun getItem(position: Int) = fragments[position].fragment
+
+        //override fun getPageTitle(position: Int) = context.getString( fragments[position].title )
+
+    }
+
 
     companion object {
         fun start(activity: Activity) = activity.startActivity(Intent(activity, MainActivity::class.java))
