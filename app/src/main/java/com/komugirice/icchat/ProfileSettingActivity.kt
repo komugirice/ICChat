37eb95e6587fe77342ea2027ef9e545acb725c3c
komugirice <iys60111118082@gmail.com>
2019-12-13 13:46:47 +0900

    ユーザ名編集、誕生日編集機能

--- app/src/main/java/com/komugirice/icchat/ProfileSettingActivity.kt
+++ app/src/main/java/com/komugirice/icchat/ProfileSettingActivity.kt
@@ -1,14 +1,22 @@
 package com.komugirice.icchat
 
+import android.app.DatePickerDialog
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.CalendarView
+import android.widget.DatePicker
+import android.widget.Toast
 import com.example.qiitaapplication.extension.getDateToString
+import com.google.firebase.firestore.FirebaseFirestore
 import com.komugirice.icchat.data.firestore.Room
 import com.komugirice.icchat.data.firestore.User
 import com.komugirice.icchat.data.firestore.manager.UserManager
 import kotlinx.android.synthetic.main.activity_profile_setting.*
+import java.time.LocalDate
+import java.util.*
 
 class ProfileSettingActivity : AppCompatActivity() {
 
@@ -28,13 +36,49 @@ class ProfileSettingActivity : AppCompatActivity() {
         val myUser = UserManager.myUser
         userId.text = myUser.userId
         userName.text = myUser.name
-        birthDay.text = myUser.birthDay?.also {it.getDateToString()}.run{"設定なし"}
+        birthDay.text = myUser.birthDay?.getDateToString() ?: "設定なし"
     }
 
     private fun initClick() {
         backImageView.setOnClickListener {
             finish()
         }
+
+        userName.setOnClickListener {
+            UserNameActivity.start(this)
+        }
+
+        birthDay.setOnClickListener {
+            showDateDialog()
+        }
+
+    }
+
+    private fun showDateDialog() {
+        val dialog = DatePickerDialog(this, object: DatePickerDialog.OnDateSetListener{
+            override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+                updateBirthDay(year, month, dayOfMonth)
+            }
+        }, 2000, 1, 1)
+        dialog.show()
+    }
+
+    fun updateBirthDay(year: Int, month: Int, dayOfMonth: Int) {
+        val birthDay = Calendar.getInstance().run {
+            set(year, month, dayOfMonth, 0, 0, 0)
+            time
+        }
+
+        FirebaseFirestore.getInstance()
+            .collection("users")
+            .document(UserManager.myUser.documentId)
+            .update("birthDay", birthDay)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    UserManager.myUser.birthDay = birthDay
+                    initLayout()
+                }
+            }
     }
 
     companion object {
---
+++ app/src/main/java/com/komugirice/icchat/UserNameActivity.kt
@@ -0,0 +1,63 @@
+package com.komugirice.icchat
+
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import com.komugirice.icchat.data.firestore.manager.UserManager
+import kotlinx.android.synthetic.main.activity_profile_setting.*
+import kotlinx.android.synthetic.main.activity_user_name.*
+import kotlinx.android.synthetic.main.activity_user_name.backImageView
+
+class UserNameActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_name)
+        initialize()
+    }
+
+    private fun initialize() {
+        initLayout()
+        initClick()
+    }
+
+    private fun initLayout() {
+        userNameEditText.setText(UserManager.myUser.name)
+
+    }
+    private fun initClick() {
+        backImageView.setOnClickListener {
+            finish()
+        }
+
+        saveButton.setOnClickListener {
+            if(userNameEditText.text.isNotEmpty())
+                update()
+        }
+
+    }
+
+    private fun update() {
+        val userName = userNameEditText.text.toString()
+        FirebaseFirestore.getInstance()
+            .collection("users")
+            .document(UserManager.myUser.documentId)
+            .update("name", userName)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    UserManager.myUser.name = userName
+                    finish()
+                }
+            }
+    }
+
+    companion object {
+        fun start(context: Context?) =
+            context?.startActivity(
+                Intent(context, UserNameActivity::class.java)
+            )
+    }
+}
