c2727535a985a562c1ff4b62c028fac86ac61b77
yss.miura <jane.was.gentleman@gmail.com>
2019-12-28 17:03:52 +0900

    Prefs has been implements

---
+++ app/src/main/java/com/komugirice/icchat/util/PrefsDemo.kt
@@ -0,0 +1,27 @@
+package com.komugirice.icchat.util
+
+import timber.log.Timber
+
+class PrefsDemo {
+
+    fun putData() {
+        Prefs().isHighQuality.put(false)
+    }
+
+    fun getData() {
+        Prefs().isHighQuality.get().blockingSingle()
+
+        Prefs().isHighQuality.get()
+            .map {
+                if (it)
+                    "HighQuality"
+                else
+                    "LowQuality"
+            }
+            .subscribe({
+                Timber.d("quality is $it")
+            }, {
+
+            })
+    }
+}
