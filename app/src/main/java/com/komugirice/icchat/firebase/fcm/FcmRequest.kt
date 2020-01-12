846bdb336f1cb6029a8b13ce8cac64edf837912f
komugirice <iys60111118082@gmail.com>
2020-01-12 14:49:51 +0900

    FcmRequest修正

--- app/src/main/java/com/komugirice/icchat/firebase/fcm/FcmRequest.kt
+++ app/src/main/java/com/komugirice/icchat/firebase/fcm/FcmRequest.kt
@@ -1,7 +1,11 @@
 package com.komugirice.icchat.firebase.fcm
 
+import com.google.gson.annotations.SerializedName
+
 data class FcmRequest (
     // var name: String = "",
+    @SerializedName("message")
     var message: String = "",
+    @SerializedName("type")
     var type: String =""
 )
