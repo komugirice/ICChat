f5ff0ee6285ddac1be85674bebff9b384850380c
komugirice <iys60111118082@gmail.com>
2020-03-11 18:25:35 +0900

    バージョンアップ画面作成

--- app/src/main/java/com/komugirice/icchat/SplashActivity.kt
+++ app/src/main/java/com/komugirice/icchat/SplashActivity.kt
@@ -1,9 +1,13 @@
 package com.komugirice.icchat
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
 import android.widget.Toast
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
@@ -18,7 +22,9 @@ import com.komugirice.icchat.util.FireStoreUtil
 import com.komugirice.icchat.util.ICChatUtil
 import kotlinx.android.synthetic.main.activity_login.*
 import timber.log.Timber
+import java.lang.Math.log
 import java.util.*
+import kotlin.math.log
 
 class SplashActivity : BaseActivity() {
 
@@ -70,7 +76,7 @@ class SplashActivity : BaseActivity() {
 
         } else {
             Handler().postDelayed({
-                LoginActivity.start(this)
+                startLoginActivity()
             }, SPLASH_TIME)
         }
     }
@@ -89,19 +95,20 @@ class SplashActivity : BaseActivity() {
     }
 
     private fun startLoginActivity() {
-        LoginActivity.start(this)
+        val adminMail = BuildConfig.ADMIN_MAIL
+        val adminPassword = BuildConfig.ADMIN_PASSWORD
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(adminMail, adminPassword)
+            .addOnCompleteListener {
+                checkVersion(){
+                    FirebaseAuth.getInstance().signOut()
+                    LoginActivity.start(this)
+                }
+        }
+
     }
 
     private fun startMainActivity() {
-        // バージョンチェック
-        FireStoreUtil.getVersion {
-            val minVersion = it
-            Timber.d("checkVersionUp minVersion:${minVersion.getVersion()} currentVersion:${BuildConfig.VERSION_NAME.getVersion()}")
-            if (minVersion.getVersion() > BuildConfig.VERSION_NAME.getVersion()) {
-                Toast.makeText(this, "アプリをアップデートしてください", Toast.LENGTH_LONG).show()
-                return@getVersion
-            }
-
+        checkVersion(){
             FirebaseFacade.initManager(onFailed()) {
                 Handler().postDelayed({
                     finishAffinity()
@@ -111,5 +118,65 @@ class SplashActivity : BaseActivity() {
         }
     }
 
+    private fun checkVersion(onSuccess: () -> Unit ) {
+
+        // バージョンチェック
+        FireStoreUtil.getVersion {
+            val minVersion = it
+            Timber.d("checkVersionUp minVersion:${minVersion.getVersion()} currentVersion:${BuildConfig.VERSION_NAME.getVersion()}")
+            if (minVersion.getVersion() > BuildConfig.VERSION_NAME.getVersion()) {
+//              // バージョン更新画面へ遷移させる
+                VersionUpActivity.start(this)
+            } else {
+                onSuccess.invoke()
+            }
+        }
+    }
+
+//    private fun promptVersionUp() {
+//        // Creates instance of the manager.
+//        val appUpdateManager = AppUpdateManagerFactory.create(this)
+//
+//        // Returns an intent object that you use to check for an update.
+//        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+//
+//        // Checks that the platform will allow the specified type of update.
+//        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+//            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+//                // For a flexible update, use AppUpdateType.FLEXIBLE
+//                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+//            ) {
+//                // Request the update.
+//                appUpdateManager.startUpdateFlowForResult(
+//                    // Pass the intent that is returned by 'getAppUpdateInfo()'.
+//                    appUpdateInfo,
+//                    // Or 'AppUpdateType.FLEXIBLE' for flexible updates.
+//                    AppUpdateType.IMMEDIATE,
+//                    // The current activity making the update request.
+//                    this,
+//                    // Include a request code to later monitor this update request.
+//                    MY_REQUEST_CODE)
+//
+//            }
+//        }
+//
+//    }
+//
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == MY_REQUEST_CODE) {
+//            if (resultCode != RESULT_OK) {
+//                Timber.d("Update flow failed! Result code: $resultCode")
+//                // If the update is cancelled or fails,
+//                // you can request to start the update again.
+//            }
+//        }
+//    }
+//
+//
+//
+//    companion object {
+//        const val MY_REQUEST_CODE = 1001
+//    }
 }
 
